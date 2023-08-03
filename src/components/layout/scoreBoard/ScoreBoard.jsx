e22a0bb445a3058cc43e67f9fabc86d3aa087c2c
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-08-03 11:20:47 -0500

    using styled components for MUI to fix pages

--- src/components/layout/scoreBoard/ScoreBoard.jsx
+++ src/components/layout/scoreBoard/ScoreBoard.jsx
@@ -1,9 +1,10 @@
-import { scoreText, playerColor, playerBorderX, playerBorderO } from "./styles";
+import { playerColor, playerBorderX, playerBorderO } from "./styles";
 import { DarkModeContext } from "@/context/DarkModeContext";
 import { ButtonGroup, Button, Box } from "@mui/material";
 import { visuallyHidden } from "@mui/utils";
 import { useContext } from "react";
 import styles from "./style.module.scss";
+import styled from "@emotion/styled";
 
 const ScoreBoard = ({ scores, xPlaying, onClick }) => {
   const { xScore, oScore } = scores;
@@ -17,17 +18,39 @@ const ScoreBoard = ({ scores, xPlaying, onClick }) => {
   const scoreX = `${playerColor}-red ${playerBorderX} ${scoreBackgroundHover}`;
   const scoreO = `${playerColor}-blue ${playerBorderO} ${scoreBackgroundHover}`;
 
+  const StyledButton = styled(Button)({
+    width: "100% !important",
+    height: "64px !important",
+    border: "0px solid transparent",
+    lineHeight: 2,
+    backgroundColor: "#0063cc",
+    borderColor: "#0063cc",
+    "&:hover": {
+      backgroundColor: "#0069d9",
+      borderColor: "#0062cc",
+      boxShadow: "none",
+    },
+    "&:active": {
+      boxShadow: "none",
+      backgroundColor: "#0062cc",
+      borderColor: "#005cbf",
+    },
+    "&:focus": {
+      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)",
+    },
+  });
+
   return (
     <div className="mx-auto">
       <ButtonGroup className="flex justify-center items-center min-w-md w-80 border-rad m-auto my-12 shadow-md">
-        <Button className={`w-full ${theme} border-t-transparent border-x-transparent border-solid border-b-4 hover:border-t-transparent hover:border-x-transparent hover:border-solid hover:border-b-4 py-4 mx-auto transition-all	ease-in-out ${scoreX} ${!xPlaying && styles.inactive}`} onClick={onClick}>
+        <StyledButton className={`${theme} border-t-transparent border-x-transparent border-solid border-b-4 hover:border-t-transparent hover:border-x-transparent hover:border-solid hover:border-b-4 py-4 mx-auto transition-all	ease-in-out ${scoreX} ${!xPlaying && styles.inactive}`} onClick={onClick}>
           X - {xScore}
           <Box sx={visuallyHidden}>X - {xScore}</Box>
-        </Button>
-        <Button className={`w-full ${theme} border-t-transparent border-x-transparent border-solid border-b-4 hover:border-t-transparent hover:border-x-transparent hover:border-solid hover:border-b-4 py-4 mx-auto transition-all	ease-in-out ${scoreO} ${xPlaying && styles.inactive}`} onClick={onClick}>
+        </StyledButton>
+        <StyledButton className={`${theme} border-t-transparent border-x-transparent border-solid border-b-4 hover:border-t-transparent hover:border-x-transparent hover:border-solid hover:border-b-4 py-4 mx-auto transition-all	ease-in-out ${scoreO} ${xPlaying && styles.inactive}`} onClick={onClick}>
           O - {oScore}
           <Box sx={visuallyHidden}> O - {oScore}</Box>
-        </Button>
+        </StyledButton>
       </ButtonGroup>
     </div>
   );
--- src/components/layout/scoreBoard/styles.js
+++ src/components/layout/scoreBoard/styles.js
@@ -1,5 +1,3 @@
-export const scoreText = ["text-base", "font-bold"];
-
 export const playerColor = "text-custom";
 export const playerBorderX = "border-b-custom-red hover:border-x-transparent";
 export const playerBorderO = "border-b-custom-blue hover:border-x-transparent";
