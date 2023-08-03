8457955a209529503691554a3d402503e9e55229
Andrew-Neely-82 <andrew.neely82@gmail.com>
2023-08-03 11:32:18 -0500

    using styled comp to work around tailwind compile issues

--- src/components/layout/scoreBoard/ScoreBoard.jsx
+++ src/components/layout/scoreBoard/ScoreBoard.jsx
@@ -21,33 +21,22 @@ const ScoreBoard = ({ scores, xPlaying, onClick }) => {
   const StyledButton = styled(Button)({
     width: "100% !important",
     height: "64px !important",
-    border: "0px solid transparent",
+    borderTop: "0px solid transparent !important",
+    borderInline: "0px solid transparent !important",
     lineHeight: 2,
-    backgroundColor: "#0063cc",
-    borderColor: "#0063cc",
-    "&:hover": {
-      backgroundColor: "#0069d9",
-      borderColor: "#0062cc",
-      boxShadow: "none",
-    },
-    "&:active": {
-      boxShadow: "none",
-      backgroundColor: "#0062cc",
-      borderColor: "#005cbf",
-    },
-    "&:focus": {
-      boxShadow: "0 0 0 0.2rem rgba(0,123,255,.5)",
+    ":hover": {
+      //
     },
   });
 
   return (
     <div className="mx-auto">
       <ButtonGroup className="flex justify-center items-center min-w-md w-80 border-rad m-auto my-12 shadow-md">
-        <StyledButton className={`${theme} border-t-transparent border-x-transparent border-solid border-b-4 hover:border-t-transparent hover:border-x-transparent hover:border-solid hover:border-b-4 py-4 mx-auto transition-all	ease-in-out ${scoreX} ${!xPlaying && styles.inactive}`} onClick={onClick}>
+        <StyledButton className={`${theme} border-solid border-b-4 hover:border-t-transparent hover:border-x-transparent hover:border-solid hover:border-b-4 py-4 mx-auto transition-all ease-in-out ${scoreX} ${!xPlaying && styles.inactive}`} onClick={onClick}>
           X - {xScore}
           <Box sx={visuallyHidden}>X - {xScore}</Box>
         </StyledButton>
-        <StyledButton className={`${theme} border-t-transparent border-x-transparent border-solid border-b-4 hover:border-t-transparent hover:border-x-transparent hover:border-solid hover:border-b-4 py-4 mx-auto transition-all	ease-in-out ${scoreO} ${xPlaying && styles.inactive}`} onClick={onClick}>
+        <StyledButton className={`${theme} border-solid border-b-4 hover:border-t-transparent hover:border-x-transparent hover:border-solid hover:border-b-4 py-4 mx-auto transition-all ease-in-out ${scoreO} ${xPlaying && styles.inactive}`} onClick={onClick}>
           O - {oScore}
           <Box sx={visuallyHidden}> O - {oScore}</Box>
         </StyledButton>
